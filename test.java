582289f2519f30e799c59fe267eb102e6d50f736
jerager <jer@cs.amherst.edu>
2016-10-21 11:22:01 -0400

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+  public static void main(String[] args) {
+  int i = 3;
+  int j = 5;
+  
+  if ( i<j )
+    if (i+15 < j)
+      System.out.println(34);
+      }
+      }
+  
